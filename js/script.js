b9f65eb5ab1fc66bf3902027f5a728bf00ceaeea
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2023-11-13 17:19:19 +0100

    add: image shows in page

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ const app = createApp({
     data(){
         return{
             message: "Message to print",
+            image: "https://i.pinimg.com/474x/4e/a2/97/4ea297707fd164ad11ec42aca9675f57.jpg"
         };
     }
 })
