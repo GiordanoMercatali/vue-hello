dad88dbde34fc50f74efcc1c4405068862a018ef
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2023-11-14 09:58:42 +0100

    refactor: mount added directly to const app

--- js/script.js
+++ js/script.js
@@ -7,6 +7,6 @@ const app = createApp({
             image: "https://i.pinimg.com/474x/4e/a2/97/4ea297707fd164ad11ec42aca9675f57.jpg"
         };
     }
-})
+}).mount("#app");
 
-app.mount("#app");
+// app.mount("#app");
